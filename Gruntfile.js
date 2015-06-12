6f7a3acee9ee13814feca8d7d5b60f7974497e60
Xavier Priour <xavier.priour@bubblyware.com>
2015-06-12 11:14:46 +0200

    added grunt tasks to ease deployment of new releases

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,14 +11,25 @@
 module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt); // Load grunt tasks automatically
 
+  var semver = require('semver');
+
   var jsFiles = [
     'Gruntfile.js',
     'tasks/*.js',
     'test/**/*.js'
   ];
 
+  var currentVersion = grunt.file.readJSON('package.json').version;
+
   // Project configuration.
   grunt.initConfig({
+    bump: {
+      options: {
+        pushTo: 'origin',
+        tagName: '%VERSION%',
+      }
+    },
+
     jshint: {
       all: jsFiles,
       options: {
@@ -40,6 +51,80 @@ module.exports = function(grunt) {
       //stageLocalIsLoaded: 'test/stageLocalIsLoaded.js',
       //hasFailed: 'test/hasFailed.js',
       //stageShouldBeEmpty: 'test/stageIsEmpty_test.js',
+    },
+
+    prompt: {
+      bump: {
+        options: {
+          questions: [
+            {
+              config:  'bump.options.setVersion',
+              type:    'list',
+              message: 'Bump version from ' + currentVersion.cyan + ' to:',
+              choices: [
+                {
+                  value: semver.inc(currentVersion, 'patch'),
+                  // name:  'Patch:  '.yellow + semver.inc('<%= pkg.version %>', 'patch').yellow +
+                  name:  ('Patch:  ' + semver.inc(currentVersion, 'patch')).yellow +
+                  '   Backwards-compatible bug fixes.'
+                },
+                {
+                  value: semver.inc(currentVersion, 'minor'),
+                  name:  ('Minor:  ' + semver.inc(currentVersion, 'minor')).yellow +
+                  '   Add functionality in a backwards-compatible manner.'
+                },
+                {
+                  value: semver.inc(currentVersion, 'major'),
+                  name:  ('Major:  ' + semver.inc(currentVersion, 'major')).yellow +
+                  '   Incompatible API changes.'
+                },
+                {
+                  value: 'custom',
+                  name:  'Custom: ?.?.?'.yellow +
+                  '   Specify version...'
+                }
+              ]
+            },
+            {
+              config:   'bump.options.setVersion',
+              type:     'input',
+              message:  'What specific version would you like',
+              when:     function(answers) {
+                return answers['bump.increment'] === 'custom';
+              },
+              validate: function(value) {
+                var valid = semver.valid(value) && true;
+                return valid || 'Must be a valid semver, such as 1.2.3-rc1. See ' +
+                  'http://semver.org/'.blue.underline + ' for more details.';
+              }
+            },
+            {
+              config:  'bump.files',
+              type:    'checkbox',
+              message: 'Apply new version to:',
+              choices: [
+                {
+                  value:   'package',
+                  name:    'package.json' +
+                  (!grunt.file.isFile('package.json') ? ' file not found, will create one'.grey : ''),
+                  checked: grunt.file.isFile('package.json')
+                },
+                {
+                  value:   'bower',
+                  name:    'bower.json' +
+                  (!grunt.file.isFile('bower.json') ? ' file not found, will create one'.grey : ''),
+                  checked: grunt.file.isFile('bower.json')
+                },
+                {
+                  value:   'git',
+                  name:    'git tag',
+                  checked: grunt.file.isDir('.git')
+                }
+              ]
+            }
+          ]
+        }
+      }
     },
 
     watch: {
@@ -70,6 +155,12 @@ module.exports = function(grunt) {
     'jshint',
     'jscs',
     'nodeunit:all',
+  ]);
+
+  grunt.registerTask('deploy', [
+    'prompt',
+    'test',
+    'bump',
   ]);
 
   // By default, lint and run all tests.
